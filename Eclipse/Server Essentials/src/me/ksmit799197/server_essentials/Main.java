348c6675399b883da2625010dc321fe4eef5a465
Kyle Smith <kyle.smithy2@yahoo.com.au>
2013-08-20 18:46:30 +1000

    Big Update - added /kill /food1 /food0 /heal
    added /kill /food1 /food0 /heal

--- Eclipse/Server Essentials/src/me/ksmit799197/server_essentials/Main.java
+++ Eclipse/Server Essentials/src/me/ksmit799197/server_essentials/Main.java
@@ -2,6 +2,7 @@ package me.ksmit799197.server_essentials;
 
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -31,16 +32,61 @@ public class Main extends JavaPlugin{
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args){
 		if(sender instanceof Player){
 			Player player = (Player) sender;
-			if(commandLabel.equalsIgnoreCase("setmylocation")){
+			if(commandLabel.equalsIgnoreCase("setwp")){
 				getConfig().set(player.getName() + ".x", player.getLocation().getBlockX());
 				getConfig().set(player.getName() + ".y", player.getLocation().getBlockY());
 				getConfig().set(player.getName() + ".z", player.getLocation().getBlockZ());
 				saveConfig();
-			}else if(commandLabel.equalsIgnoreCase("location")){
+			}else if(commandLabel.equalsIgnoreCase("tpwp")){
 				int x = getConfig().getInt(player.getName() + ".x"), y = getConfig().getInt(player.getName() + ".y"), z = getConfig().getInt(player.getName() + ".z");
 				player.teleport(new Location(player.getWorld(), x, y, z));
+			}else if(commandLabel.equalsIgnoreCase("heal") || commandLabel.equalsIgnoreCase("h")){
+				if(args.length == 0){
+					player.setHealth(20);
+					player.setFireTicks(0);
+					player.sendMessage(ChatColor.GREEN + "You Have Been Healed!");
+				}else if(args.length == 1){
+					if(player.getServer().getPlayer(args [0])!=null){
+					Player targetPlayer = player.getServer().getPlayer(args [0]);
+					targetPlayer.setHealth(20);
+					targetPlayer.setFireTicks(0);
+					targetPlayer.sendMessage(ChatColor.GREEN + " You Have Been Healed!");
+					}else{
+						player.sendMessage(ChatColor.RED + "[ERROR] That Player is Not Online!");
+					}
+				}	
+			}else if(commandLabel.equalsIgnoreCase("food1")){
+				if(args.length == 0){
+					player.setFoodLevel(20);
+					player.sendMessage(ChatColor.GREEN + "Your Hunger has been Satisfied!");	
+				}
+				else if(args.length == 1){
+					if(player.getServer().getPlayer(args [0])!=null){
+						Player targetPlayer = player.getServer().getPlayer(args [0]);
+						targetPlayer.setFoodLevel(20);
+						player.sendMessage(ChatColor.GREEN + "Set Player's Food to 20!");
+						targetPlayer.sendMessage(ChatColor.GREEN + "Your Hunger has been Satisfied!");
+					}
+					else{
+						player.sendMessage(ChatColor.RED + "[ERROR] That Player is Not Online!");
+					}
+				}	
+				}else if(commandLabel.equalsIgnoreCase("food0")){
+				if(args.length == 0){
+					player.setFoodLevel(0);
+					player.sendMessage(ChatColor.GREEN + "Your Suddenly Starving!");	
+				}
+				else if(args.length == 1){
+					if(player.getServer().getPlayer(args [0])!=null){
+						Player targetPlayer = player.getServer().getPlayer(args [0]);
+						targetPlayer.setFoodLevel(0);
+						player.sendMessage(ChatColor.GREEN + "Set Player's Food to 0!");
+						targetPlayer.sendMessage(ChatColor.GREEN + "Your Suddenly Starving!");
+					}else{
+						player.sendMessage(ChatColor.RED + "[ERROR] That Player is Not Online!");
+					}
+				}
 			}
 		}return false;
 	}
-
 }
